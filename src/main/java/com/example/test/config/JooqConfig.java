05b55095f04f41e1d5d90c17b54df423c9998b5e
Serhiy200 <gusaksergiy710@gmail.com>
2021-10-28 10:36:33 +0300

    update Jooq config

--- src/main/java/com/example/test/config/JooqConfig.java
+++ src/main/java/com/example/test/config/JooqConfig.java
@@ -1,16 +1,15 @@
 package com.example.test.config;
 
+import org.jooq.ConnectionProvider;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultDSLContext;
 import org.postgresql.ds.PGSimpleDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -19,40 +18,39 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan({"com.example.test.jooq.public_.tables"})
 @EnableTransactionManagement
-@PropertySource("classpath:application.properties")
 public class JooqConfig {
 
-    @Autowired
-    private Environment environment;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String userName;
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @Bean
     public DataSource dataSource() {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setUrl(environment.getRequiredProperty("spring.datasource.url"));
-        dataSource.setUser(environment.getRequiredProperty("spring.datasource.username"));
-        dataSource.setPassword(environment.getRequiredProperty("spring.datasource.password"));
+        dataSource.setUrl(url);
+        dataSource.setUser(userName);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
     @Bean
-    public TransactionAwareDataSourceProxy transactionAwareDataSource() {
-        return new TransactionAwareDataSourceProxy(dataSource());
+    public TransactionAwareDataSourceProxy transactionAwareDataSource(DataSource dataSource) {
+        return new TransactionAwareDataSourceProxy(dataSource);
     }
 
     @Bean
-    public DataSourceConnectionProvider connectionProvider() {
-        return new DataSourceConnectionProvider(transactionAwareDataSource());
+    public DataSourceConnectionProvider connectionProvider(TransactionAwareDataSourceProxy transactionAwareDataSourceProxy) {
+        return new DataSourceConnectionProvider(transactionAwareDataSourceProxy);
     }
 
     @Bean
-    public DefaultDSLContext dsl() {
-        return new DefaultDSLContext(configuration());
-    }
-
-    public DefaultConfiguration configuration() {
+    public DefaultDSLContext dsl(ConnectionProvider connectionProvider) {
         DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
-        jooqConfiguration.set(connectionProvider());
+        jooqConfiguration.set(connectionProvider);
         jooqConfiguration.set(SQLDialect.POSTGRES);
-        return jooqConfiguration;
+        return new DefaultDSLContext(jooqConfiguration);
     }
 }
